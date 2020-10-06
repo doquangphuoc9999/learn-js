3aab5b5d5f4334b517904d493c72f41efd6ffcbc
doquangphuoc9999 <doquangphuoc9999@gmail.com>
2020-10-06 13:38:59 +0700

    add BT 1

---
+++ TH- Hiển thị đầu ra/my-js.js
@@ -0,0 +1,13 @@
+function showmesseger() {
+  alert("Xin chào");
+}
+
+function exercise() {
+  document.getElementById("exercise").innerHTML = "Xin chào!";
+}
+function exercise3() {
+  document.write("Xin chào");
+}
+function exercise4() {
+  console.log("Xin chào");
+}
